dab9f85ddd61b28eb8dc2be727aacfe5b0022710
Joseline Ly <joseline.cly@gmail.com>
2025-03-27 12:12:36 -0700

    finalized Program2, created output.txt to store output

--- Program2.java
+++ Program2.java
@@ -1,16 +1,28 @@
 import java.io.*;
 import java.util.*;
+import java.text.Normalizer;
 
 public class Program2 {
     public static void main (String[] args) {
-        fileRead();
+        try {
+            fileRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void fileRead() {
+    // Warning "resource" for HashMap raw type
+    // Warning "unused" for variable k in lambda expression
+    @SuppressWarnings({ "resource", "unused" })
+    public static void fileRead() throws IOException {
         Scanner scnr = new Scanner(System.in);
-        System.out.print("Enter a file: ");
+        System.out.print("Enter an input file: ");
         String filename = scnr.nextLine();
         BufferedReader reader = null;
+
+        System.out.print("Enter an output file: ");
+        String outputFilename = scnr.nextLine();
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilename));
 
         Map<String, List<String>> groups = new HashMap();
         String word = "";
@@ -21,25 +33,26 @@ public class Program2 {
             String line;
             while ((line = reader.readLine()) != null) {
                 word = line.toLowerCase().replace("'", "").trim();
+                word = removeAccents(word);
                 if (word.isEmpty()) {
                     continue;
                 }
                 freqCount = getCharCount(word);
-
-                // groups.computeIfAbsent(freqCount, k -> new ArrayList<>()).add(line);
-                System.out.println(word);
+                groups.computeIfAbsent(freqCount, k -> new ArrayList<>()).add(line);
             }
         }
         
         catch (Exception e) {
             System.out.print("Error while opening file.");
+            e.printStackTrace();
         }
 
         // For each list of strings in Map groups, check if its size is greater than 1.
-        // If it is, print the entire list of anagrams.
+        // If it is, print the entire list of anagrams. Otherwise, skip since group is not anagram.
         for (List<String> group : groups.values()) {
             if (group.size() > 1) {
-                System.out.println(group);
+                writer.write(group.toString());
+                writer.newLine(); // Move to the next line
             }            
         }
         scnr.close();
@@ -56,6 +69,14 @@ public class Program2 {
 
         // Convert frequency array into string of numbers and return
         return Arrays.toString(freq);
+    }
+
+    private static String removeAccents(String input) {
+        if (input == null) {
+            return null;
+        }
+        String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+        return normalized.replaceAll("\\p{M}", "");
     }
     
 }
---
+++ README.txt
@@ -0,0 +1,16 @@
+How to Compile/Run:
+1) Select an IDE of your choosing.
+2) Download Program2.java.
+3) Run Program2.java.
+4) Enter input file name in terminal (words.txt).
+5) Enter output file name in terminal (output.txt).
+5) Open output.txt to see results of Program2.
+
+Disclaimer:
+For this program, apostrophes are removed during presorting to normalize
+the string. For instance, plane's and planes would be considered the same
+word, and therefore included in the same anagram grouping.
+
+Additionally, accented characters were normalized to characters without
+accents during the presorting process. For their frequency hashes, they are
+considered the same as their unaccented character.
